0fe453217365648281dcf6128ce4a64462107008
Сергей <gromoverzhetss@mail.ru>
2026-08-13 20:34:41 +0300

    На проверку

--- test/test_ui.py
+++ test/test_ui.py
@@ -1,4 +1,4 @@
-
+import pytest
 from page.page_ui import SchedulePage
 import allure
 
@@ -6,26 +6,29 @@ import allure
 @allure.feature("ТЕСТ")
 @allure.story("Создание события")
 @allure.title("Создание личного события на русском языке")
+@pytest.mark.test_ui
 def test_create_rus_event(auth):
     try:
         page = SchedulePage(auth)
-        page.create_event(title="Дипломка")
+        page.create_event_rus(title="Дипломка")
     finally:
         auth.quit()
 
 @allure.feature("ТЕСТ")
 @allure.story("Создание события")
 @allure.title("Создание личного события на с названием и описанием на английском")
+@pytest.mark.test_ui
 def test_create_eng_event(auth):
     try:
         page = SchedulePage(auth)
-        page.create_event(title="Diploma", description="Final work")
+        page.create_event_en(title="Diploma", description="Final work")
     finally:
         auth.quit()
 
 @allure.feature("ТЕСТ")
 @allure.story("Изменение масштаба")
 @allure.title("Изменение масштаба интерфейса")
+@pytest.mark.test_ui
 def test_change_name_event(auth):
     try:
         page = SchedulePage(auth, wait_time=5)
@@ -35,7 +38,8 @@ def test_change_name_event(auth):
 
 @allure.feature("ТЕСТ")
 @allure.story("Убрать видимость личных событий")
-@allure.title("Убрать галочку чтобы личные события непоказывались в расписании")
+@allure.title("Убрать галочку чтобы личные события не показывались в расписании")
+@pytest.mark.test_ui
 def test_unplug_event(auth):
     try:
         page = SchedulePage(auth, wait_time=5)
@@ -43,13 +47,5 @@ def test_unplug_event(auth):
     finally:
         auth.quit()
 
-@allure.feature("ТЕСТ")
-@allure.story("Удаление события")
-@allure.title("Выбрать нужное событие и удалить")
-def test_delete_event(auth):
-    try:
-        page = SchedulePage(auth, wait_time=5)
-        page.delete_event()
-    finally:
-        auth.quit()
+
 
