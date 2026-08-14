343eda2f457994aefbe3f1a2b1c787f0a11cf61f
Сергей <gromoverzhetss@mail.ru>
2026-08-14 15:11:47 +0300

    На проверку

--- page/page_ui.py
+++ page/page_ui.py
@@ -1,8 +1,10 @@
-from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import allure
+
+
 
 
 class SchedulePage:
@@ -12,7 +14,8 @@ class SchedulePage:
     INPUT_TITLE = (By.XPATH, "//input[@placeholder='Например: посмотреть вебинар']")
     TEXTAREA_DESCRIPTION = (By.CSS_SELECTOR, "textarea[placeholder='Например: ссылка на вебинар']")
     BTN_SAVE = (By.XPATH, "//button[contains(@class, '-type-primary') and contains(@class, '-color-brand')]")
-    CLICK_EV = (By.XPATH, "(//div[@class='passed-event-cover'])[11]")
+    CLICK_EV_RUS = (By.XPATH, "//tcc-calendar-event-personal[contains(., 'Дипломка')]")
+    CLICK_EV_EN = (By.XPATH, "//tcc-calendar-event-personal[contains(., 'Diploma')]")
     BUT_DEL = (By.CSS_SELECTOR, ".root.-type-secondary.-color-brand.-size-m.-active")
     BTN_SETTINGS = (By.CSS_SELECTOR, ".cog-btn")
     GRID = (By.CSS_SELECTOR, '[style="width: 896px; height: 1248px;"]')
@@ -38,7 +41,7 @@ class SchedulePage:
             element = self.wait.until(EC.element_to_be_clickable(self.SPAN_PERSONAL_EVENT))
             element.click()
 
-    def create_event(self, title, description=None):
+    def create_event_rus(self, title, description=None):
         with allure.step("Ждем и кликаем кнопку добавления"):
             self.click_add_event()
             self.select_personal_event()
@@ -58,10 +61,62 @@ class SchedulePage:
             save_btn = self.wait.until(EC.element_to_be_clickable(self.BTN_SAVE))
             save_btn.click()
         with allure.step("Проверяем что событие создано"):
+            dip_ev = self.wait.until(EC.visibility_of_element_located(self.CLICK_EV_RUS))
+            return dip_ev
+
+
+    def delete_event_rus(self):
+        with allure.step("Находим нужное событие и нажимаем на него"):
+            clear = self.wait.until(EC.visibility_of_element_located(self.CLICK_EV_RUS))
+            clear.click()
+        with allure.step("Находим и нажимаем на кнопку удалить"):
+            button = self.wait.until(EC.element_to_be_clickable(self.BUT_DEL))
+            button.click()
+        with allure.step("Проверяем что событие удалено"):
             toast_locator = (By.CSS_SELECTOR, ".-type-positive")
             self.wait.until(EC.visibility_of_element_located(toast_locator))
             if toast_locator:
-                print("Событие создано")
+                print("Событие удалено")
+
+
+
+
+    def create_event_en(self, title, description=None):
+        with allure.step("Ждем и кликаем кнопку добавления"):
+            self.click_add_event()
+            self.select_personal_event()
+
+        with allure.step("Находим поле для ввода названия"):
+            title_field = self.wait.until(EC.visibility_of_element_located(self.INPUT_TITLE))
+        with allure.step("Вводим название события"):
+            title_field.send_keys(title)
+
+        if description:
+            with allure.step("Находим поле для описания"):
+                descr_field = self.wait.until(EC.visibility_of_element_located(self.TEXTAREA_DESCRIPTION))
+            with allure.step("Вводим текст"):
+                descr_field.send_keys(description)
+
+        with allure.step("Жмём на кнопку 'Сохранить'"):
+            save_btn = self.wait.until(EC.element_to_be_clickable(self.BTN_SAVE))
+            save_btn.click()
+        with allure.step("Проверяем что событие создано"):
+            dip_ev = self.wait.until(EC.visibility_of_element_located(self.CLICK_EV_EN))
+            return dip_ev
+
+
+    def delete_event_en(self):
+        with allure.step("Находим нужное событие и нажимаем на него"):
+            clear = self.wait.until(EC.visibility_of_element_located(self.CLICK_EV_EN))
+            clear.click()
+        with allure.step("Находим и нажимаем на кнопку удалить"):
+            button = self.wait.until(EC.element_to_be_clickable(self.BUT_DEL))
+            button.click()
+        with allure.step("Проверяем что событие удалено"):
+            toast_locator = (By.CSS_SELECTOR, ".-type-positive")
+            self.wait.until(EC.visibility_of_element_located(toast_locator))
+            if toast_locator:
+                print("Событие удалено")
 
 
     def change_scale(self):
@@ -73,8 +128,7 @@ class SchedulePage:
             scale_option.click()
         with allure.step("Проверка того что интерфейс уменьшен"):
             grid = self.wait.until(EC.visibility_of_element_located(self.GRID))
-            if grid:
-                print("Интерфейс уменьшен")
+            return grid
 
     def toggle_hide_personal_events(self):
         with allure.step("Проверка что календарь отображается"):
@@ -88,19 +142,9 @@ class SchedulePage:
             checkbox = self.wait.until(EC.visibility_of_element_located(self.CHECKBOX_HIDE_EVENTS))
             checkbox.click()
         with allure.step("Проверка того что календарь скрыт"):
-            calendar = self.wait.until(EC.invisibility_of_element(self.INV_CAL))
-            if calendar :
-                print("Календарь не отображается")
+            calendar = self.wait.until(EC.invisibility_of_element_located(self.INV_CAL))
+            if calendar:
+                print("Календарь скрыт")
+            return calendar
 
-    def delete_event(self):
-        with allure.step("Находим нужное событие и нажимаем на него"):
-            clear = self.wait.until(EC.visibility_of_element_located(self.CLICK_EV))
-            clear.click()
-        with allure.step("Находим и нажимаем на кнопку удалить"):
-            button = self.wait.until(EC.element_to_be_clickable(self.BUT_DEL))
-            button.click()
-        with allure.step("Проверяем что событие удалено"):
-            toast_locator = (By.CSS_SELECTOR, ".-type-positive")
-            self.wait.until(EC.visibility_of_element_located(toast_locator))
-            if toast_locator:
-                print("Событие удалено")
+
--- test/test_ui.py
+++ test/test_ui.py
@@ -8,44 +8,44 @@ import allure
 @allure.title("Создание личного события на русском языке")
 @pytest.mark.test_ui
 def test_create_rus_event(auth):
-    try:
-        page = SchedulePage(auth)
-        page.create_event_rus(title="Дипломка")
-    finally:
-        auth.quit()
+    page = SchedulePage(auth)
+    event_rus = page.create_event_rus(title="Дипломка")
+    text = event_rus.text.strip()
+    assert text.startswith("Дипломка")
+    page.delete_event_rus()
+
 
 @allure.feature("ТЕСТ")
 @allure.story("Создание события")
 @allure.title("Создание личного события на с названием и описанием на английском")
 @pytest.mark.test_ui
 def test_create_eng_event(auth):
-    try:
-        page = SchedulePage(auth)
-        page.create_event_en(title="Diploma", description="Final work")
-    finally:
-        auth.quit()
+    page = SchedulePage(auth)
+    event_en = page.create_event_en(title="Diploma", description="Final work")
+    text = event_en.text.strip()
+    assert text.startswith("Diploma")
+    page.delete_event_en()
+
+
 
 @allure.feature("ТЕСТ")
 @allure.story("Изменение масштаба")
 @allure.title("Изменение масштаба интерфейса")
 @pytest.mark.test_ui
 def test_change_name_event(auth):
-    try:
-        page = SchedulePage(auth, wait_time=5)
-        page.change_scale()
-    finally:
-        auth.quit()
+    page = SchedulePage(auth, wait_time=5)
+    change = page.change_scale()
+    assert change.is_displayed()
 
 @allure.feature("ТЕСТ")
 @allure.story("Убрать видимость личных событий")
 @allure.title("Убрать галочку чтобы личные события не показывались в расписании")
 @pytest.mark.test_ui
 def test_unplug_event(auth):
-    try:
-        page = SchedulePage(auth, wait_time=5)
-        page.toggle_hide_personal_events()
-    finally:
-        auth.quit()
+    page = SchedulePage(auth, wait_time=5)
+    hide = page.toggle_hide_personal_events()
+    assert hide
+
 
 
 
